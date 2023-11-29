e9c98b33352f2c53655be8b2af9a897c1e913eb2
Hopeson <hopesonogaga@gmail.com>
2023-11-29 11:08:54 +0100

    new ui

--- build/js/main.js
+++ build/js/main.js
@@ -1,53 +1,40 @@
 //
-const close_menu = document.querySelector('.close-menu')
-const open_menu = document.querySelector('.open-menu')
-const menu_options = document.querySelector('.menu-options')
+const close_menu = document.querySelector(".close-menu");
+const open_menu = document.querySelector(".open-menu");
+const menu_options = document.querySelector(".menu-options");
+const doc_card = document.querySelector(".documet-card");
+const more_card = document.querySelector(".more-card");
+const doc_test = document.querySelector(".document-test");
+const menu_test = document.querySelector(".menu-test");
 
-menu_options.classList.add('hidden')
-
-open_menu.addEventListener('click', function(){
-    console.log('yo')
-    open_menu.classList.add('hidden')
-    close_menu.classList.remove('hidden')
-    menu_options.classList.remove('hidden')
-})
-
-
-close_menu.addEventListener('click', function(){
-  console.log('yo')
-  close_menu.classList.add('hidden')
-  open_menu.classList.remove('hidden')
-  menu_options.classList.add('hidden')
-})
-
-// email section
-
-
-
-
-
+menu_options.classList.add("hidden");
 
 
 
 // mapn
-let map = document.getElementById("map");
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      let map = L.map("map").setView([51.5272401, -0.0912403], 13);
+// let map = document.getElementById("map");
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       let map = L.map("map").setView([51.5272401, -0.0912403], 13);
 
-      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+//       L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//       }).addTo(map);
 
-      L.marker([51.5272401, -0.0912403])
-        .addTo(map)
-        .bindPopup("prosure solutions limited ")
-        .openPopup();
-    },
-    function () {
-      alert("please turn on location");
-    }
-  );
-}
+//       L.marker([51.5272401, -0.0912403])
+//         .addTo(map)
+//         .bindPopup("11th Hour Innovation Properties LTD")
+//         .openPopup();
+
+//         L.marker([51.5272368, -0.0886654])
+//         .addTo(map)
+//         .bindPopup("11th Hour Innovation Properties LTD")
+//         .openPopup()
+//     },
+//     function () {
+//       console.log('bug of')
+//     }
+//   );
+// }
